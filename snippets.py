67ab7c88474210d6cbbd1775b5a1ccf114165ef6
Ben Altieri <chiefspace@gmail.com>
2016-09-29 11:44:35 +0000

    Fixed get() if statment to test for row instead of snippet

--- snippets.py
+++ snippets.py
@@ -4,29 +4,61 @@ __author__      = "Ben Altieri"
 
 import logging
 import argparse
+import psycopg2
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect(database="snippets")
+logging.debug("Database connection established.")
 
 def put(name, snippet):
-    """
-    Store a snippet with an associated name.
+    """Store a snippet with an associated name."""
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
 
-    Returns the name and the snippet
-    """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    with connection, connection.cursor() as cursor:
+        try:
+            command = "insert into snippets values (%s, %s)"
+            cursor.execute(command, (name, snippet))
+        except psycopg2.IntegrityError as e:
+            connection.rollback()
+            command = "update snippets set message=%s where keyword=%s"
+            cursor.execute(command, (snippet, name))
+
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
     return name, snippet
 
 def get(name):
-    """Retrieve the snippet with a given name.
+    """Retrieve the snippet message for a given snippet keyword."""
+    logging.info("Retrieving snippet for requested keyword: {!r}".format(name))
 
-    If there is no such snippet, return '404: Snippet Not Found'.
+    with connection, connection.cursor() as cursor:
+        cursor.execute("select message from snippets where keyword=%s", (name,))
+        row = cursor.fetchone()
 
-    Returns the snippet.
-    """
-    logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
+    if not row:
+        # No snippet was found with that name.
+        return "404: Snippet Not Found"
+    else:
+        return row
 
+def catalog():
+    logging.info("Creating a catalog view of all snippets stored in the database")
+
+    with connection, connection.cursor() as cursor:
+        cursor.execute("select * from snippets order by keyword")
+        stored_snippets = cursor.fetchall()
+        return stored_snippets
+
+def search(string_to_search):
+    logging.info("Searching all snippets stored in the database for substring contained in any snippet message")
+
+    with connection, connection.cursor() as cursor:
+        like_string = str("%%" + string_to_search + "%%")
+        cursor.execute("select * from snippets where message like %s", (like_string,))
+        results_of_search = cursor.fetchall()
+        return results_of_search
 
 def main():
     """Main function"""
@@ -46,6 +78,15 @@ def main():
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="Name of the snippet")
 
+    # Subparser for the catalog command
+    logging.debug("Constructing catalog subparser")
+    catalog_parser = subparsers.add_parser("catalog", help="Create a catalog view of snippets stored in the database")
+
+    # Subparser for the search command
+    logging.debug("Constructing search subparser")
+    search_parser = subparsers.add_parser("search", help="Search all snippets stored in the database for substring contained in any snippet messages")
+    search_parser.add_argument("string_to_search", help="String to search within the message portion of the snippet")
+
     arguments = parser.parse_args()
     # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
@@ -53,10 +94,18 @@ def main():
 
     if command == "put":
         name, snippet = put(**arguments)
-        print("Stored {!r} as {!r}".format(snippet, name))
+        print("Attempted to store {!r} as {!r}".format(snippet, name))
     elif command == "get":
         snippet = get(**arguments)
-        print("Retrieved snippet: {!r}".format(snippet))
+        print("Snippet retrieval results: {!r}".format(snippet))
+    elif command == "catalog":
+        catalog_of_snippets = catalog()
+        print("Creating a catalog view of all snippets stored in the database")
+        print(catalog_of_snippets)
+    elif command == "search":
+        search_results = search(**arguments)
+        print("Searching all snippets stored in the database for substring contained in any snippet message")
+        print(search_results)
 
 if __name__ == "__main__":
     main()
